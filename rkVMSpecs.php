e32e9d8be213c32e951be0d0b49b2b8e3d2ef608
Frederic Lhoest <flhoest@gmail.com>
2021-03-26 15:58:57 +0100

    Create rkVMSpecs.php
    Get VM specs from snapshot

---
+++ rkVMSpecs.php
@@ -0,0 +1,33 @@
+<?php
+		
+	/*			__________        ___.            .__  __    
+					\______   \ __ __ \_ |__  _______ |__||  | __
+					 |       _/|  |  \ | __ \ \_  __ \|  ||  |/ /
+					 |    |   \|  |  / | \_\ \ |  | \/|  ||    < 
+					 |____|_  /|____/  |___  / |__|   |__||__|_ \
+					    		\/             \/                  \/ Php Framework
+	*/
+
+	// Include section
+	include_once "rkFramework.php";
+
+	// User credentials	
+	$clusterConnect=array(
+				"username" => "username",
+				"password" => "password",
+				"ip" => "192.168.1.1"
+				
+				);
+
+  // Entry Point
+
+  $VMname="vmName";
+	$vmSpecs=rkGetESXVMConfig($clusterConnect,$VMname);
+
+	print("\nMachine ".rkColorOutput($VMname)." in snapshot ".rkColorOutput($vmSpecs["snapID"])." has the following specs :\n");
+	print("\t".rkColorOutput($vmSpecs["cpu"])." vCPU\n");
+	print("\t".rkColorOutput($vmSpecs["memory"]." GB")." RAM\n");
+	print("\t".rkColorOutput(rkformatBytes($vmSpecs["disk"]))." HDD\n");
+	print("\n");
+
+?>
